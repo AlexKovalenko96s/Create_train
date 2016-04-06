b757b0c90d64a5eb825bd8890e82b5c1e04b0f1f
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-04-06 13:39:40 +0300

    set two button: "delete train" and "delete booking"

--- Create trains/src/ua/kas/createTrains/Controller.java
+++ Create trains/src/ua/kas/createTrains/Controller.java
@@ -17,32 +17,60 @@ public class Controller {
 	@FXML TextField textTIME_OUT;
 	@FXML TextField textTIME_IN;
 	@FXML TextField textSEATS;
+	@FXML TextField textNUMBER;
 	
 	public void add(ActionEvent e) throws SQLException{
-		String t = "1";
 		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/freemove", "root", "root");
 		java.sql.PreparedStatement myStmt = myConn
-				.prepareStatement("insert into train(city_out,city_in,time_out,time_in,date,train) values (?,?,?,?,?,?)");
+				.prepareStatement("insert into train(city_out,city_in,time_out,time_in,date,¹) values (?,?,?,?,?,?)");
 		myStmt.setString(1, textCITY_OUT.getText());
 		myStmt.setString(2, textCITY_IN.getText());
 		myStmt.setString(3, textTIME_OUT.getText());
 		myStmt.setString(4, textTIME_IN.getText());
 		myStmt.setString(5, textDATE.getText());
-		myStmt.setString(6, t);
+		myStmt.setString(6, textNUMBER.getText());
 		myStmt.executeUpdate();
 		System.out.println("Complet!");		
 	}
 	
-	public void dell(ActionEvent e) throws SQLException{
+	public void dell_train(ActionEvent e) throws SQLException{
 		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/freemove", "root", "root");
-		String deleteSQL = "delete from train where city_out=? and city_in=? and time_out=? and time_in=? and date=? ";
-		PreparedStatement preparedStatement = myConn.prepareStatement(deleteSQL);
-		preparedStatement.setString(1, textCITY_OUT.getText());
-		preparedStatement.setString(2, textCITY_IN.getText());
-		preparedStatement.setString(3, textTIME_OUT.getText());
-		preparedStatement.setString(4, textTIME_IN.getText());
-		preparedStatement.setString(5, textDATE.getText());
-		preparedStatement.executeUpdate();
-		System.out.println("Complet!");		
+		String deleteSQL_train  = "delete from train where city_out=? and city_in=? and time_out=? and time_in=? and date=? and ¹=? ";
+		PreparedStatement preparedStatement_train = myConn.prepareStatement(deleteSQL_train);
+		preparedStatement_train.setString(1, textCITY_OUT.getText());
+		preparedStatement_train.setString(2, textCITY_IN.getText());
+		preparedStatement_train.setString(3, textTIME_OUT.getText());
+		preparedStatement_train.setString(4, textTIME_IN.getText());
+		preparedStatement_train.setString(5, textDATE.getText());
+		preparedStatement_train.setString(6, textNUMBER.getText());
+		preparedStatement_train.executeUpdate();
+		
+		String deleteSQL_booking = "delete from seats where city_out=? and city_in=? and time_out=? and time_in=? and date=? and ¹=?";
+		PreparedStatement preparedStatement_booking = myConn.prepareStatement(deleteSQL_booking);
+		preparedStatement_booking.setString(1, textCITY_OUT.getText());
+		preparedStatement_booking.setString(2, textCITY_IN.getText());
+		preparedStatement_booking.setString(3, textTIME_OUT.getText());
+		preparedStatement_booking.setString(4, textTIME_IN.getText());
+		preparedStatement_booking.setString(5, textDATE.getText());
+		preparedStatement_booking.setString(6, textNUMBER.getText());
+		preparedStatement_booking.executeUpdate();
+		
+		System.out.println("Complet!");	
+	}
+	
+	public void dell_booking(ActionEvent e) throws SQLException{
+		
+		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/freemove", "root", "root");
+		String deleteSQL_booking = "delete from seats where city_out=? and city_in=? and time_out=? and time_in=? and date=? and ¹=?";
+		PreparedStatement preparedStatement_booking = myConn.prepareStatement(deleteSQL_booking);
+		preparedStatement_booking.setString(1, textCITY_OUT.getText());
+		preparedStatement_booking.setString(2, textCITY_IN.getText());
+		preparedStatement_booking.setString(3, textTIME_OUT.getText());
+		preparedStatement_booking.setString(4, textTIME_IN.getText());
+		preparedStatement_booking.setString(5, textDATE.getText());
+		preparedStatement_booking.setString(6, textNUMBER.getText());
+		preparedStatement_booking.executeUpdate();
+		
+		System.out.println("Complet!");	
 	}
 }
